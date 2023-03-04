17453b80574f72e5ce95e5417bbfb2782194a672
lumerocode <luismelendez0894@gmail.com>
2023-03-04 10:32:09 -0500

    changes

---
+++ proyecto/src/components/CartWidget.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaCartPlus } from "react-icons/fa";
+
+
+function CartWidget(){
+    return(
+        <div>
+            <a href="" target="_blank"><label class="social__link"><FaCartPlus/></label></a>
+            
+        </div>
+    )
+}
+
+export default CartWidget;
--- proyecto/src/components/NavBar.js
+++ proyecto/src/components/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../scss/layout/_header.scss';
 import Logo from '../assets/logo.png'
+import CartWidget from './CartWidget';
 
 function NavBar(){
     return(
 
+        <>
         <div>
             <header class="header">
             <section class="header__section">
@@ -21,24 +23,30 @@ function NavBar(){
 
                 <nav id="main-menu" class="header__nav">
                     <ul class="header__nav__items">
-                        <li class="header__nav__link"> <a>Home</a> </li>
+                        <li class="header__nav__link header__nav__link--selected"> <a>Home</a> </li>
                         <li class="header__nav__link"> <a>Portfolio</a> </li>
                         <li class="header__nav__link"> <a>Services</a> </li>
-                        <li class="header__nav__link header__nav__link--selected"> <a>About</a> </li>
+                        <li class="header__nav__link "> <a>About</a> </li>
                         <li class="header__nav__link"> <a>Contact</a> </li> 
                     </ul>
                 </nav>
                 {/* Utilizo la clase social por que tambien la utilizaré en el footer y en cada  miembro del team */}
-                <ul class="social">
+                {/* <ul class="social">
                     <a href="https://www.facebook.com/" target="_blank"><label class="social__link"><i class="fa fa-facebook"></i></label></a>
                     <a href="https://www.linkedin.com/" target="_blank"><label class="social__link"><i class="fa fa-linkedin"></i></label></a>
                     <a href="https://www.instagram.com/" target="_blank"><label class="social__link"><i class="fa fa-instagram"></i></label></a>
                     <a href="https://www.twitter.com/" target="_blank"><label class="social__link"><i class="fa fa-twitter"></i></label></a>
-                </ul>
+                </ul> */}
+
+                <CartWidget/>
                 
             </section>
             </header>
         </div>
+
+       
+
+        </>
 
     )
 }
